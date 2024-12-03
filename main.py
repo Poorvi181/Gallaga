4d8cf0a78212d3ba9dba763bd9d15b6714308151
Poorvi181 <mannarchana@yahoo.co.uk>
2024-12-03 19:01:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ HEIGHT=600
 speed=5
 gallaga=Actor("player")
 gallaga.pos=(WIDTH/2,HEIGHT-50)
-bug=Actor("bug")
-bug.pos=(WIDTH/2,50)
+bugs=[]
+for col in range(8):
+    for row in range(4):
+        bugs.append(Actor("bug.png"))
+        bugs[-1].x=100+(50*col)
+        bugs[-1].y=80+(50*row)
 bullets=[]
 def draw():
     screen.fill("black")
     gallaga.draw()
-    bug.draw()
+    for bullet in bullets:
+        bullet.draw()
+    for b in bugs:
+        b.draw()
+
 def update():
     if keyboard.left:
         gallaga.x-=speed
@@ -21,7 +29,16 @@ def update():
         gallaga.x+=speed
         if gallaga.x>WIDTH:
             gallaga.x=WIDTH
+    for bullet in bullets:
+        if bullet.y<0:
+            bullets.remove(bullet)
+        else:
+            bullet.y-=10
+
 def on_key_down(key):
     if key==keys.SPACE:
         bullets.append(Actor("bullet"))
+        bullets[-1].x=gallaga.x
+        bullets[-1].y=gallaga.y
+
 pgzrun.go()

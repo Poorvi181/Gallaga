7b094ee8840fee591e906128606199ef736232a9
Poorvi181 <mannarchana@yahoo.co.uk>
2024-12-08 14:58:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pgzrun
 TITLE="Gallaga"
 WIDTH=1200
 HEIGHT=600
+score=0
+direction=1
 speed=5
 gallaga=Actor("player")
 gallaga.pos=(WIDTH/2,HEIGHT-50)
+gallaga.dead=False
+gallaga.countdown=90
 bugs=[]
 for col in range(8):
     for row in range(4):
@@ -14,13 +18,16 @@ for col in range(8):
 bullets=[]
 def draw():
     screen.fill("black")
-    gallaga.draw()
+    if gallaga.dead==False:
+        gallaga.draw()
     for bullet in bullets:
         bullet.draw()
     for b in bugs:
         b.draw()
 
 def update():
+    global direction,score
+    movedown=False
     if keyboard.left:
         gallaga.x-=speed
         if gallaga.x<0:
@@ -34,6 +41,23 @@ def update():
             bullets.remove(bullet)
         else:
             bullet.y-=10
+    if len(bugs)>0 and (bugs[0].x<80 or bugs[-1].x>WIDTH-80):
+        direction=direction*(-1)
+        movedown=True
+    
+        
+    for bug in bugs:
+        bug.x+=4*direction
+        for bullet in bullets:
+            if bullet.colliderect(bug):
+                bugs.remove(bug)
+                bullets.remove(bullet)
+                score+=1
+                sounds.sound.play()
+        if movedown:
+            bug.y+=20
+        if bug.colliderect(gallaga):
+            gallaga.dead=True
 
 def on_key_down(key):
     if key==keys.SPACE:
